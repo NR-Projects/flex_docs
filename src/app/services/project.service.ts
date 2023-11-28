1af36cce728abd94d591f0b16d8cbafb77a2672a
Tuna <98098486+Null-Root@users.noreply.github.com>
2023-11-28 17:10:35 +0800

    fix board not deleted bug when deleting project

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -1,14 +1,16 @@
 import { Injectable } from '@angular/core';
 import { Firestore, addDoc, collection, collectionData, deleteDoc, doc, docData, query, updateDoc, where } from '@angular/fire/firestore';
-import { Project } from '../models/project.model';
-import { Observable, map } from 'rxjs';
+import { Board, Project } from '../models/project.model';
+import { Observable, firstValueFrom, map } from 'rxjs';
+import { BoardService } from './board.service';
 
 @Injectable({
 	providedIn: 'root'
 })
 export class ProjectService {
 
-	constructor(private firestore: Firestore) { }
+	constructor(private firestore: Firestore,
+		private boardService: BoardService) { }
 
 	async addProject(project: Project): Promise<void> {
 		let projectCollection = collection(this.firestore, "projects");
@@ -37,6 +39,14 @@ export class ProjectService {
 	}
 
 	async deleteProject(project: Project): Promise<void> {
+		// Delete all under boards
+		let boardsPromise = await firstValueFrom(this.boardService.getAllProjectBoards(project.id!));
+		boardsPromise.forEach(async board => {
+			// Delete Board
+			await this.boardService.deleteProjectBoard(project.id!, board);
+		});
+
+		// Delete Project
 		let docRef = doc(this.firestore, "projects", project.id!);
 		await deleteDoc(docRef);
 	}
